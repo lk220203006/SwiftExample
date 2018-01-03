7abb59aac6104d95d0805ee9ac9a6d91c751fb9c
liukun <lk220203006@126.com>
2018-01-03 23:26:02 +0800

    convenience

--- WeiBoWithSB/WeiBoWithSB/class/Main/MainViewController.swift
+++ WeiBoWithSB/WeiBoWithSB/class/Main/MainViewController.swift
@@ -10,7 +10,8 @@ import UIKit
 
 class MainViewController: UITabBarController {
 
-    private lazy var composeBtn:UIButton = UIButton()
+//    private lazy var composeBtn:UIButton = UIButton.createButton(imageName: "", bgImageName: "tabbar_mid")
+    private lazy var composeBtn:UIButton = UIButton(imageName: "", bgImageName: "tabbar_mid")
     override func viewDidLoad() {
         super.viewDidLoad()
         setupComposeBtn()
@@ -21,9 +22,7 @@ class MainViewController: UITabBarController {
 extension MainViewController{
     private func setupComposeBtn(){
         tabBar.addSubview(composeBtn)
-        composeBtn.setBackgroundImage(UIImage(named: "tabbar_mid"), for: .normal)
-        composeBtn.setBackgroundImage(UIImage(named: "tabbar_mid"), for: .highlighted)
-        composeBtn.sizeToFit()
+        
         composeBtn.center = CGPoint.init(x: tabBar.center.x, y: tabBar.bounds.size.height*0.5)
     }
 }
---
+++ WeiBoWithSB/WeiBoWithSB/class/Tools/Categary/UIButton-Extension.swift
@@ -0,0 +1,33 @@
+//
+//  UIButton-Extension.swift
+//  WeiBoWithSB
+//
+//  Created by liukun on 2018/1/3.
+//  Copyright © 2018年 liukun. All rights reserved.
+//
+
+import Foundation
+import UIKit
+
+extension UIButton{
+    //swift中类方法是以class开头的方法 类似于OC中+开头的方法
+    class func createButton(imageName:String,bgImageName:String) -> UIButton {
+        let btn = UIButton()
+        btn.setBackgroundImage(UIImage(named: bgImageName), for: .normal)
+        btn.setBackgroundImage(UIImage(named: bgImageName), for: .highlighted)
+        btn.sizeToFit()
+        return btn
+    }
+    
+    //convenice:便利，使用convenience修饰的构造函数叫做便利构造函数
+    //便利构造函数通常用在对系统的类进行构造函数的扩充时使用
+    //便利构造函数通常都是卸载extension里面
+    //便利构造函数init前面需要加载convenience
+    //在便利构造函数中需要明确调用self.init（）
+    convenience init (imageName:String,bgImageName:String){
+        self.init()
+        setBackgroundImage(UIImage(named: bgImageName), for: .normal)
+        setBackgroundImage(UIImage(named: bgImageName), for: .highlighted)
+        sizeToFit()
+    }
+}
